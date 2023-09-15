f84c65261ec40fd7cfd4d9122760c942eef9f995
gutsserrano <gutsserrano@gmail.com>
2023-09-15 14:47:36 -0300

    calculadora finalizada

--- aula7_14_setembro/calculadora/script/script.js
+++ aula7_14_setembro/calculadora/script/script.js
@@ -4,6 +4,8 @@ var botoes = document.querySelectorAll('.numeros');
 
 var operadores = document.querySelectorAll('.operacao');
 
+var ponto = document.querySelector('#ponto');
+
 var valor1, valor2, sinal;
 
 //NodeList
@@ -15,6 +17,10 @@ botoes.forEach(function(botao){
         visor.value += botao.textContent;
     })
 })
+
+ponto.onclick = () => {
+    visor.value += ponto.textContent;
+}
 
 operadores.forEach((operadores) => {
 
@@ -46,9 +52,33 @@ calculo.onclick = () => {
             visor.value = valor1 * valor2;    
 
             break;
-        case '/':
+        case '÷':
             visor.value = valor1 / valor2;    
 
             break;
+        
     }
+}
+
+var raiz = document.querySelector('.raiz');
+
+raiz.onclick = () => {
+    valor1 = parseFloat(visor.value);
+    visor.value = Math.sqrt(valor1) ;    
+}
+
+var clear = document.querySelector('.clear');
+
+clear.onclick = () => {
+    valor1 = 0;
+    valor2 = 0;
+    visor.value = '';
+}
+
+var off = document.querySelector('.off');
+
+off.onclick = () => {
+    valor1 = 0;
+    valor2 = 0;
+    visor.value = '';
 }
